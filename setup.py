e43b7b151c9d8509296b43c1481250343a70eeac
Al Fajr <62805347+aN4ksaL4y@users.noreply.github.com>
2021-11-06 17:29:52 +0700

    adding dependencies

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 long_description = open('README.md').read()
 setup(
+	version='0.0.4',
 	url='https://github.com/aN4ksaL4y/mytelegraph',
 	name='mytelegraph',
 	author='Cingkariak Sawah',
@@ -9,7 +10,7 @@ setup(
 	description='a tiny helper for posting to a Telegra.ph page.',
 	long_description = long_description,
 	long_description_content_type='text/markdown',
-	version='0.0.3', 
+	install_requires=['icecream', 'requests'],
 	packages=['telegraph'], 
 	scripts=['bin/posting']
 )
