5a6b87a3d432203e80c646c49ba6919cf5438834
Your Name <you@example.com>
2021-11-06 16:53:40 +0700

    prepare PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+long_description = open('README.md').read()
 setup(
+	url='https://github.com/aN4ksaL4y/mytelegraph',
 	name='mytelegraph',
 	author='Cingkariak Sawah',
 	author_email='fajrim228@gmail.com',
 	description='a tiny helper for posting to a Telegra.ph page.',
-	version='0.0.2', 
+	long_description = long_description,
+	long_description_content_type='text/markdown',
+	version='0.0.3', 
 	packages=['telegraph'], 
 	scripts=['bin/posting']
 )
